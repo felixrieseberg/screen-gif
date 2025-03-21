bd358860157cc93c9383804d3a18fbeebab75694
Felix Rieseberg <fr@makenotion.com>
2025-03-21 05:08:20 -0700

    Error silently if not present

--- bin/cli.js
+++ bin/cli.js
@@ -6,6 +6,17 @@ const { spawn } = require('child_process');
 // Get the path to our compiled binary
 const binaryPath = path.join(__dirname, 'GifViewer');
 
+// If the binary doesn't exist, do nothing
+if (!fs.existsSync(binaryPath)) {
+  if ((process.env.DEBUG || "").includes("screen-gif")) {
+    console.log('GifViewer helper app not found. Make sure it was compiled during installation.');
+    process.exit(1);
+  }
+
+  // Otherwise, exit silently
+  process.exit(0);
+}
+
 // Spawn the binary with all the arguments passed to this script
 const child = spawn(binaryPath, process.argv.slice(2), {
   stdio: 'inherit'
